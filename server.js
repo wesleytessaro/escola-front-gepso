08bc3157c2b6e856c81c1bc25a6c05bec3c73ae5
Wesley Tessaro <wesley.andrade@novaandradina.org>
2021-08-30 07:33:18 -0400

    fix some erros

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express=require('express');
+const app=express ();
 
-const app = express();
+const PORT = process.env.PORT || 8080;
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/escola-front'));
+app.use(express.static(__dirname +'/dist/escola-front'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/escola-front/'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.get('/*',(req,res)=> {
+  res.sendFile(__dirname + '/dist/escola-front/index.html');
+});
